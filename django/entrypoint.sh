b6f7c0d0e7882cc6a06f5f865805f70e55697f06
WYV-2022 <105045159+WYV-2022@users.noreply.github.com>
2023-08-01 19:33:24 +0530

    bug fix

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -4,7 +4,7 @@
 # Replace 'postgres' with your PostgreSQL service name in docker-compose.yml
 # Change the '5' to a higher value if you encounter connection issues
 # This helps ensure that the database is up before starting Django
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$DB_PASSWORD psql -h "postgres" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 5
 done
@@ -17,5 +17,3 @@ python manage.py collectstatic --noinput
 
 # Start the Django development server
 python manage.py runserver 0.0.0.0:8000
-
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "myproject.wsgi:application"]
