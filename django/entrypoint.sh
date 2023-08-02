37d805a6ea0dd53f311273f5990cce66d8db7fcb
VOID <rushitmananwyv2022@gmail.com>
2023-08-02 14:59:41 +0530

    fix

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -4,16 +4,6 @@
 # Replace 'postgres' with your PostgreSQL service name in docker-compose.yml
 # Change the '5' to a higher value if you encounter connection issues
 # This helps ensure that the database is up before starting Django
-until PGPASSWORD=$DB_PASSWORD psql -h "postgres" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 5
-done
-
-# Apply database migrations
-python manage.py migrate
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Start the Django development server
-python manage.py runserver 0.0.0.0:8000
+echo "insideentrypoint"
+python django/manage.py migrate &&
+python django/manage.py runserver 0.0.0.0:8000
